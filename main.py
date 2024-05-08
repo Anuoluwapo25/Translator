b0e21e11313ce87b53ecc5351a7138d9e79552c7
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-05-08 15:35:51 -0700

    flast appp updated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def landing():
@@ -10,6 +12,7 @@ def landing():
 @app.route('/signup')
 def signup():
     return "<h1>Signup Page</h1><p>This is the signup page. Please fill out the form to sign up.</p>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
