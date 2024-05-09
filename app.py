de03cef3860fea81461d87b97140cf6dba2e29da
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-05-08 23:48:51 -0700

    flask

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from googletrans import Translator
 
-
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='./template')
 
 @app.route('/')
-def landing():
-    return render_template("index.html")
+def index():
+    return render_template('index.html')
 
 
 @app.route('/signup')
 def signup():
-    return "<h1>Signup Page</h1><p>This is the signup page. Please fill out the form to sign up.</p>"
+    return render_template('sign.html')
 
+
+@app.route('/translate', methods=['POST', 'GET'])
+def translate():
+    text = request.json['text']
+    target_language = request.json['target_language']
+
+    translator = Translator()
+    translations = {}
+    translations[target_language] = translator.translate(text, dest=target_language).text
+
+    return jsonify(translations)
 
 if __name__ == '__main__':
     app.run(debug=True)
