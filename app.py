245300da64313b366db880a8a9e4556cac73acd3
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-05-10 04:50:04 -0700

    hidden files

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
 from flask import Flask, render_template, request, jsonify
 from googletrans import Translator
 
-app = Flask(__name__, template_folder='./template')
+app = Flask(__name__, template_folder='template', static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/signup')
 def signup():
-    return render_template('sign.html')
+    return render_template('signup.html')
 
-
-@app.route('/translate', methods=['POST', 'GET'])
+@app.route('/translate', methods=['POST'])
 def translate():
-    text = request.json['text']
-    target_language = request.json['target_language']
+    if request.method == 'POST':
+        data = request.get_json()
+        text = data.get('text')
+        target_language = data.get('target_language')
 
-    translator = Translator()
-    translations = {}
-    translations[target_language] = translator.translate(text, dest=target_language).text
+        if not text or not target_language:
+            return jsonify({'error': 'Invalid request'})
 
-    return jsonify(translations)
+        translator = Translator()
+        translation = translator.translate(text, dest=target_language).text
+
+        return jsonify({'translation': translation})
+    else:
+        return jsonify({'error': 'Method not allowed'})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
